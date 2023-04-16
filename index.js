4bd54f5084d398c614d5712889c164a3bac0591b
Berkay Bideci <berkaybideci@gmail.com>
2023-04-17 00:34:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find(function (record) {
+        return record.result === 'W'
+    });
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
